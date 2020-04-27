7194823bd1c853f5c9645c2cb93e61fc47dcd739
churabou <ginga7888@gmail.com>
2020-04-27 20:47:34 +0900

    Step3 Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,43 @@
 import React from "react"
-export default () => <h1>Hello Gatsby!</h1>
+import { graphql, Link } from "gatsby"
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            tags
+            date(formatString: "YYYY年MM月DD日")
+          }
+        }
+      }
+    }
+  }
+`
+
+export default ({ data }) => {
+  const edges = data.allMarkdownRemark.edges
+  return (
+    <div>
+      <h1>全ての記事</h1>
+      <ul>
+        {edges.map((edge) => (
+          <li key={edge.node.id}>
+            <Link to={edge.node.id}>
+              <div>
+                {edge.node.frontmatter.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
+                ))}
+              </div>
+              <div>{edge.node.frontmatter.title}</div>
+              <div>{edge.node.frontmatter.date}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
